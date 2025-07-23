58ef414b6def4dde5f3cac51897bffcf788bd137
Rishabh <rishabhrai2907@gmail.com>
2025-07-23 21:43:52 +0530

    frontend

---
+++ src/pages/admin/ListBookings.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ListBookings = () => {
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const[bookings,setBookings] = useState([]);
+  const[isloading,setIsLoading] = useState(true);
+
+  
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ListBookings
